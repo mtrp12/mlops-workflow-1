cbcb1d6b9a029c982a698c064fe3ee5fb041288a
mtrp12 <6695345+mtrp12@users.noreply.github.com>
2024-11-27 21:08:58 +0600

    update

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from src.data.data_ingestion import DataIngestion
 from src.features.data_transformation import DataTransformation
 import pandas as pd
 
+from src.models.model_trainer import ModelTrainer
+from src.pipelines.training_pipeline import CustomerChurnPipeline
+
 try:
-    data_ingestion = DataIngestion()
-    train_path, test_path = data_ingestion.initiate_data_ingestion()
-    data_transformation = DataTransformation()
-    x_train, y_train, x_test, y_test, preprocess_path = data_transformation.initiate_data_transformation(train_path, test_path)
+    CustomerChurnPipeline().run_pipeline()
 except Exception as e:
     logging.error("Error here. Exiting", exc_info=e)
